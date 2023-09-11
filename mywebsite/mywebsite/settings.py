f55daad74f0b8d1311ef8fbae5f0ff8291ff68dc
paulaji <paulajiparayil123@gmail.com>
2023-09-11 14:50:36 +0530

    installed channels

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # add the channels we just pip installed
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
